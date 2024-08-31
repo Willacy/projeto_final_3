59ad801921f01105e897d5ba68e08fb46eaf00ad
Willacy <wannicy@gmail.com>
2024-08-30 21:17:09 -0300

    index

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+    session_start();
+
+    require_once "./php/view/usuario.php";
+    require_once "./php/view/tela_home.php";
+    require_once "./php/controller/controller.php";
+    
+    
+    $uri = $_SERVER['REQUEST_URI'];
+    $method = $_SERVER['REQUEST_METHOD'];
+    $uri = explode("?",$uri)[0];
+    $uri = preg_replace('/\/{2,}/','/',$uri);
+    $uri = explode("/",$uri);
+    $uri = array_splice($uri, 2);
+    $tamanho = count ($uri);
+    if ($uri[$tamanho-1]=="")
+        array_pop($uri);
+    $inicio = 0;
+    
+    $usuario = new Usuario();
+    $home = new Home();
+    
+    if (isset($_SESSION['mensagem'])){
+        echo "
+            <div class='toast show position-fixed bottom-0 end-0 align-items-center text-bg-info border-0 text-white'  role='alert' aria-live='assertive' aria-atomic='true'>
+            <div class='d-flex'>
+                <div class='toast-body'>
+                {$_SESSION['mensagem']}
+                </div>
+                <button type='button' class='btn-close btn-close-white me-2 m-auto' data-bs-dismiss='toast' aria-label='Close'></button>
+            </div>
+            </div>";
+    }
+
+    unset($_SESSION['mensagem']);
+
+    if ($method=="GET" and $uri[$inicio]=="login" and count($uri)==1)
+        //Exibe formulario de Login
+        $usuario->getLogin ();
+    else if ($method=="POST" and $uri[$inicio]=="login" and count($uri)==1)
+    {
+        //Valida o login
+        $usuario->postLogin ();
+    }
+    else if ($method=="GET" and $uri[$inicio]=="home" and count($uri)==1 and $_SESSION["validar"] == true)
+    {
+        //Se Login ok, vai para Home
+        $home->home ();
+    }
+    else if ($method=="POST" and $uri[$inicio]=="usuario" and count($uri)==1 and $_SESSION["validar"] == true)
+    {
+        //
+        $usuario->post ();
+    }else if ($method == "GET" and $uri[$inicio] == "usuario" and count($uri) == 1 and $_SESSION["validar"] == true) {
+        $usuario->getCadastro();
+    }
+
+    else
+    {
+        echo "<h1>Página não encontrada</h1>";
+        $resposta = ['uri'=>$uri,'method'=>$method];
+        echo json_encode($resposta);
+    }
+
+    
+?>
---
+++ sql/inserts.sql
@@ -0,0 +1,1 @@
+insert into usuarios values (null, 'Willacy Santana', 'LBSoul', '9dc12dd4719748642edc8e9382b3043b6715e815');
