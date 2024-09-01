1352d081fd48422fb2e8fa25f4636a74087524b2
Willian-Q-Silva <202016360037@ifba.edu.br>
2024-09-01 15:29:26 -0300

    implementação da pesquisa

--- php/view/usuario.php
+++ php/view/usuario.php
@@ -85,6 +85,61 @@ class Usuario extends Home
         }
     }
 
+    public function postPesquisaUsuario(){
+        $this->cabecalho();
+        $this->navbar();
+         // Instancia o ControllerUser e chama o método pesquisarUsuario
+         $controllerUser = new ControllerUser();
+         if (isset($_POST['criterio'])) {
+         $resultados = $controllerUser->pesquisarUsuario($_POST['criterio']);
+         }else{
+            $resultados = $controllerUser->pesquisarUsuario(' ');
+         }
+         
+         ?>
+
+    <h1>Pesquisa de Usuário</h1>
+
+    <form method="post" action="">
+        <label for="criterio">Pesquisar por Nome ou Login:</label>
+        <input type="text" id="criterio" name="criterio" required>
+        <button type="submit">Pesquisar</button>
+    </form>
+
+    <?php if (isset($resultados)): ?>
+        <?php if ($resultados): ?>
+            <h2>Resultados da Pesquisa:</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nome</th>
+                        <th>Login</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($resultados as $usuario): ?>
+                        <tr>
+                            <td><?= htmlspecialchars($usuario['id_usuario']); ?></td>
+                            <td><?= htmlspecialchars($usuario['nome_usuario']); ?></td>
+                            <td><?= htmlspecialchars($usuario['login_usuario']); ?></td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        <?php else: ?>
+            <p>Nenhum usuário encontrado com o critério especificado.</p>
+        <?php endif; ?>
+    <?php endif; ?>
+</body>
+</html>
+
+<?php
+         
+    }
+    public function getPesquisaUsuario(){
+        $this->postPesquisaUsuario();
+    }
 
     public function put()
     {
