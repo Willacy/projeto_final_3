596b2293c07e757130ba0e3ded1d38d06bb3938f
Willacy <wannicy@gmail.com>
2024-09-12 14:25:04 -0300

    update form livros

--- php/view/livro.php
+++ php/view/livro.php
@@ -11,25 +11,29 @@ class Livro extends Home
         <div class="container">
             <h2>Cadastro de Livro</h2>
             <form action="/projeto_final_3/livro" method="POST">
-                <div class="form-group my-3">
+                <div class="form-floating mb-3">
+                    <input type="text" name="titulo_livro" id="titulo_livro" class="form-control" placeholder="Título do livro"
+                        required>
                     <label for="titulo_livro">Título do Livro</label>
-                    <input type="text" name="titulo_livro" id="titulo_livro" class="form-control" required>
                 </div>
-                <div class="form-group my-3">
+                <div class="form-floating mb-3">
+                    <input type="number" name="valor_venda_livro" id="valor_venda_livro" class="form-control"
+                        placeholder="Valor de Venda" required>
                     <label for="valor_venda_livro">Valor de Venda</label>
-                    <input type="text" name="valor_venda_livro" id="valor_venda_livro" class="form-control" required>
                 </div>
-                <div class="form-group my-3">
+                <div class="form-floating mb-3">
+                    <input type="number" name="valor_aluguel_livro" id="valor_aluguel_livro" class="form-control"
+                        placeholder="Valor de Aluguel" required>
                     <label for="valor_aluguel_livro">Valor de Aluguel</label>
-                    <input type="text" name="valor_aluguel_livro" id="valor_aluguel_livro" class="form-control" required>
                 </div>
-                <div class="form-group my-3">
+                <div class="form-floating mb-3">
+                    <input type="text" name="isbn_livro" id="isbn_livro" class="form-control" placeholder="ISBN" required>
                     <label for="isbn_livro">ISBN</label>
-                    <input type="text" name="isbn_livro" id="isbn_livro" class="form-control" required>
                 </div>
-                <div class="form-group my-3">
+                <div class="form-floating mb-3">
+                    <input type="number" name="quantidade_livro" id="quantidade_livro" class="form-control"
+                        placeholder="Quantidade de Livro" required>
                     <label for="quantidade_livro">Quantidade de Livro</label>
-                    <input type="text" name="quantidade_livro" id="quantidade_livro" class="form-control" required>
                 </div>
                 <button type="submit" class="btn btn-primary">Salvar</button>
             </form>
@@ -54,7 +58,7 @@ class Livro extends Home
 
         $sql = "SELECT * FROM livros";
         $result = Controller::getConexao()->query($sql);
-        
+
         ?>
         <div class="container">
             <h2>Lista de Livros</h2>
@@ -70,16 +74,16 @@ class Livro extends Home
                     </tr>
                 </thead>
                 <tbody>
-                <?php while ($row = $result->fetch_assoc()) { ?>
-                    <tr>
-                        <td><?= $row['id_livro'] ?></td>
-                        <td><?= $row['titulo_livro'] ?></td>
-                        <td><?= $row['autor_livro'] ?></td>
-                        <td><?= $row['valor_aluguel_livro'] ?></td>
-                        <td><?= $row['ano_public_livro'] ?></td>
-                        <td><?= $row['isbn_livro'] ?></td>
-                    </tr>
-                <?php } ?>
+                    <?php while ($row = $result->fetch_assoc()) { ?>
+                        <tr>
+                            <td><?= $row['id_livro'] ?></td>
+                            <td><?= $row['titulo_livro'] ?></td>
+                            <td><?= $row['autor_livro'] ?></td>
+                            <td><?= $row['valor_aluguel_livro'] ?></td>
+                            <td><?= $row['ano_public_livro'] ?></td>
+                            <td><?= $row['isbn_livro'] ?></td>
+                        </tr>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
